00db41fadc87c9a44a239a2b35190c40d866d0c1
unknown <k21cb256@cnc.kyusan-u.ac.jp>
2023-06-02 11:15:23 +0900

    新規

--- jsadv/advmapdata.js
+++ jsadv/advmapdata.js
@@ -4,4 +4,25 @@ let map = [
     ['└','┼','┬','┤'],
     ['','├','┴','┼','─','┐'],
     ['S','┴','K','┘','','S'],
-]
+];
+
+let images = {
+    '┐':'left_down',
+    '─':'left_right',
+    '┬':'left_right_down',
+    '┘':'left_up',
+    '┴':'left_up_right',
+    '┤':'left_up_down',
+    '┼':'left_up_right_down',
+    '┌':'right_down',
+    '│':'up_down',
+    '└':'up_right',
+    '├':'up_right_down',
+    'G':'goal',
+    'S':'deadend',
+    'K':'key',
+    'D':'door',
+};
+
+let x = 2;
+let y = 3;
