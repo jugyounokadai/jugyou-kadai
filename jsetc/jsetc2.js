d7caa67d09c2c8eed2f10bebe21473094ef2963f
unknown <k21cb256@cnc.kyusan-u.ac.jp>
2023-06-19 15:31:03 +0900

    新規

--- jsetc/jsetc2.js
+++ jsetc/jsetc2.js
@@ -6,13 +6,13 @@ btn.addEventListener('click',() => {
     let age = parseInt(inp.value);
     if(isNaN(inp.value)){
         elem.innerText = "半角数字で正しい年齢を入れてください";   
-    } else  ( age < 18 )
-        elem.innerText = "未成年";         
-             if (6< age <=15 ){
-                elem.innerText = "未成年（義務教育期間）";
-    } else if( age >=65　){
+    } else if ( age < 18 ) {
+        elem.innerText = "未成年"; 
+    }  if ( 6< age < 16 ) {
+        elem.innerText = "未成年(義務教育期間)"; 
+    } else if ( age >=65　){
         elem.innerText = "高齢者";
-    }　else{
+    }　else {
         elem.innerText = "成人";
     }
 } );
